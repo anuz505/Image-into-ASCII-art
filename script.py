dab796416c5e44a6f43bc982020b211ced3f7ac7
anuj bhandari <anujb364@gmail.com>
2024-03-10 22:10:44 +0545

    final

--- script.py
+++ script.py
@@ -2,12 +2,14 @@
 import PIL.Image
 
 #ascii character used to build the output text
-ASCII_CHARS = ["N","@","#","W","9","8","7","6","5","4","3","2","1","0","?","!","a","b","c",";",":","+","=","-",",","_"]
+ASCII_CHARS = ["@","#","$","%","?","*","+",";",":",",","."]
 #resize the image according to a new width 
-def resize_image(image, new_width=100, new_height=100):
+def resize_image(image,new_width=100):
     width, height = image.size
-    resized_image = image.resize((new_width, new_height))
-    return resized_image
+    ratio = height / width
+    new_height = int (new_width *ratio)
+    resized_image = image.resize((new_width,new_height))
+    return (resized_image)
 
 #convert each pixel into grayscale
 def grayify (image):
@@ -18,11 +20,11 @@ def grayify (image):
 def pixels_to_ascii(image):
     pixels = image.getdata()
     characters = "".join([ASCII_CHARS[int(pixel / (256 / len(ASCII_CHARS)))] for pixel in pixels])
-
     return characters
 
 # ...
-def main(new_width=100, new_height=50):
+
+def main(new_width=100):
     # Initialize the 'image' variable
     image = None
 
@@ -34,16 +36,15 @@ def main(new_width=100, new_height=50):
         print(f"{path} is not a valid pathname to an image. Error: {e}")
         return 
 
-    # Resizing the image with the new width and height
-    new_image_data = pixels_to_ascii(grayify(resize_image(image, new_width, new_height)))
+    #converting image into ASCII
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
 
-    # Formatting
+    #format
     pixel_count = len(new_image_data)
     ascii_image = "\n".join(new_image_data[i:i+new_width] for i in range(0, pixel_count, new_width))
     print(ascii_image)
 
-    # Saving the result to ASCII image text
-    with open("ascii_image.txt", "w") as f:
+    #now saving the result to ascii image text
+    with open ("ascii_image.txt", "w") as f:
         f.write(ascii_image)
-
-main(new_width=80, new_height=40)
+main()
